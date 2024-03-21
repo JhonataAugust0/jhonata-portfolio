7b55df619c53644c9db4b01e89e629c406aa9716
JhonataAugust0 <jhon.augustosilva@gmail.com>
2024-03-21 15:23:35 -0300

    Correção da responsividade, correção do bug de exibição do nome, criação de componente botão e utilização do options api do vue 3

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,87 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+interface CustomButtonProps {
+  buttonText: string;
+  buttonColor: string;
+  buttonHeight: string;
+  buttonWidth: string;
+  buttonBorderColor: string;
+  buttonBorderRadius?: string;
+  buttonTextColor?: string;
+  handleClick?: Function;
+}
+
+export default defineComponent({
+  name: 'CustomButton',
+  props: {
+    buttonText: {
+      type: String,
+      required: true
+    },
+    buttonColor: {
+      type: String,
+      default: 'var(--var-color-code-block-blue)'
+    },
+    buttonHeight: {
+      type: String,
+      default: '38px'
+    },
+    buttonWidth: {
+      type: String,
+      default: '72px'
+    },
+    buttonBorderRadius: {
+      type: String,
+      default: '4px'
+    },
+    buttonBorderColor: {
+      type: String,
+      default: '1px solid var(--var-color-code-block-blue)'
+    },
+    buttonTextColor: {
+      type: String,
+      default: '#3d5264'
+    },
+    handleClick: {
+      type: Function,
+      required: false,
+    }
+  },
+  data (props: CustomButtonProps) {
+    const buttonProps = props;
+    return {
+      buttonProps,
+    }
+  },
+});
+</script>
+
+<template>
+  <button
+    class="custom-button"
+    :style="{
+      backgroundColor: buttonProps.buttonColor,
+      height: buttonProps.buttonHeight,
+      width: buttonProps.buttonWidth,
+      borderRadius: buttonProps.buttonBorderRadius,
+      border: buttonProps.buttonBorderColor,
+      color: buttonTextColor,
+    }"
+    @click="buttonProps.handleClick"
+  >
+    {{ buttonText }}
+  </button>
+</template>
+
+<style>
+.custom-button {
+  display: flex;                                        /* Define os botões como elementos flexíveis */
+  align-items: center;                                  /* Centraliza o texto verticalmente dentro dos botões */
+  justify-content: center;                              /* Centraliza o texto horizontalmente dentro dos botões */
+  padding: 8px 16px;                                    /* Adiciona preenchimento aos botões */
+  font-family: Inter, monospace;                        /* Define a fonte dos botões */
+  cursor: pointer;                                      /* Altera o cursor para um ponteiro ao passar sobre os botões */
+  transition: background-color 0.3s ease;               /* Adiciona uma transição suave à cor de fundo dos botões */
+}
+</style>
